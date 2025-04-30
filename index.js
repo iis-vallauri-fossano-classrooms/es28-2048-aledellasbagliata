6ff8cc6af855a0cc87bda2a9fd68592fccf379f1
aledellasbagliata <g.peano.3590@vallauri.edu>
2025-04-30 11:43:00 +0200

    matrice e inizio a gestire i tasti

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.onload = function () {
 
    creazioneMatrice();
 
-   
+
 
 }
 
@@ -21,11 +21,67 @@ function creazioneMatrice()
 
          div.id = i + "-" + j;
 
+         div.tabIndex = 0;
+         document.addEventListener("keydown", gestioneTasti)
+
          wrapper.appendChild(div);
       }
    }
+
+   let iPos1 = generaNumero(0,3);
+   let jPos1 = generaNumero(0,3);
+
+   const pos1Casuale = document.getElementById(iPos1 + "-" + jPos1);
+
+   pos1Casuale.innerText = "2";
+
+   let iPos2;
+   let jPos2
+   do
+   {
+      iPos2 = generaNumero(0,3);
+      jPos2 = generaNumero(0,3);
+
+   }while(iPos1 == iPos2 && jPos1 == jPos2)
+   
+   const pos2Casuale = document.getElementById(iPos2 + "-" + jPos2);
+   pos2Casuale.innerText = "2";
+
 }
 
 function generaNumero(min, max) {
    return Math.floor((max - min) * Math.random()) + min;
+}
+
+function gestioneTasti(e)
+{
+   switch(e.key)
+   {
+      case "ArrowUp":
+         frecciaSu();
+         break;
+      case "ArrowDown":
+         break;
+      case "ArrowLeft":
+         break;
+      case "ArrowRight":
+         break;
+   }
+}
+
+
+function frecciaSu()
+{
+   for(let j = 0; j < DIM; j++)
+   {
+      for(let i = 0; i < DIM; i++)
+      {
+         let currentCell = document.getElementById(i + "-" + j);
+
+         if(currentCell.innerText == "")
+         {
+            currentCell.innerText = document.getElementById(i + "-" + j + 1).innerText;
+         }
+      }
+   }
 }
